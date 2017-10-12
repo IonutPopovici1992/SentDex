c523161af0f85a4634e8ee09dc9e6882ad7c65f6
John Wayne <ionutpopovici92@gmail.com>
2017-10-12 23:59:37 +0300

    classes.py + getting_user_input.py

---
+++ Python_3_Basics/classes.py
@@ -0,0 +1,22 @@
+class calculator:
+
+    def addition(x, y):
+        added = x + y
+        print(added)
+
+    def substraction(x, y):
+        substracted = x - y
+        print(substracted)
+
+    def multiplication(x, y):
+        multiplied = x * y
+        print(multiplied)
+
+    def division(x, y):
+        divided = x / y
+        print(divided)
+
+calculator.addition(1, 2)
+calculator.substraction(1, 2)
+calculator.multiplication(1, 2)
+calculator.division(1, 2)
