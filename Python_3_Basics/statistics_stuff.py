bca249f4f57a09acaecdc438f752f76a0c336bd4
John <ionut_popovici92@yahoo.com>
2017-12-16 23:59:35 +0200

    Added statistics_stuff.py

---
+++ Python_3_Basics/statistics_stuff.py
@@ -0,0 +1,19 @@
+import statistics
+
+example_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Mean
+x = statistics.mean(example_list)
+print(x)
+
+# Median
+y = statistics.median(example_list)
+print(y)
+
+# Standard Deviation
+z = statistics.stdev(example_list)
+print(z)
+
+# Variance
+w = statistics.variance(example_list)
+print(w)
