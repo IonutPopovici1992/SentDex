f495b82e70ac3bd1bd1885e53cc6fc91f2be8e25
John <ionut_popovici92@yahoo.com>
2017-12-18 01:26:21 +0200

    Added list_manipulation.py

---
+++ Python_3_Basics/list_manipulation.py
@@ -0,0 +1,33 @@
+x = [1, 2, 3, 4, 5]
+y = ['Janet', 'Jessy', 'Bob', 'Adam', 'Chris', 'George', 'Ema']
+
+# append
+x.append(6)
+print(x)
+
+# insert
+x.insert(6, 7)
+print(x)
+
+# remove
+x.remove(6)
+print(x)
+
+# slice
+print(x[0:2])
+print(x[2:4])
+
+# last element
+print(x[-1])
+
+# index
+print(x.index(1))
+
+# count
+print(x.count(1))
+
+# sort
+x.sort()
+print(x)
+y.sort()
+print(y)
