44f8af9e324a9fbb63bcd9163f0ff8022f836b67
John <ionut_popovici92@yahoo.com>
2017-12-18 21:06:48 +0200

    Added try_and_except.py

---
+++ Python_3_Basics/try_and_except.py
@@ -0,0 +1,32 @@
+import csv
+
+with open('example.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+
+    colors = []
+    dates = []
+
+    for row in readCSV:
+        color = row[3]
+        date = row[0]
+
+        colors.append(color)
+        dates.append(date)
+
+    print(colors)
+    print(dates)
+
+    try:
+        whatColor = input('What color do you wish to know the date of? ')
+
+        if whatColor in colors:
+            color_index = colors.index(whatColor.lower())
+            theDate = dates[color_index]
+            print('The date of',whatColor,'is:',theDate,'.')
+        else:
+            print('Color not found, or is not a color!')
+
+    except Exception as e:
+        print(e)
+
+    print('Continuing ...')
