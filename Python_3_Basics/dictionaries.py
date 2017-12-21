01fbc04148ee8d4c587294519d15794c5d4f631d
John <ionut_popovici92@yahoo.com>
2017-12-21 02:54:12 +0200

    Added dictionaries.py

---
+++ Python_3_Basics/dictionaries.py
@@ -0,0 +1,11 @@
+exampleDictionary = {'Jack': 15, 'Bob': 22, 'Alice': 12, 'Kevin': 17}
+
+print(exampleDictionary)
+print(exampleDictionary['Jack'])
+exampleDictionary['Tim'] = 14
+print(exampleDictionary)
+exampleDictionary['Tim'] = 15
+print(exampleDictionary)
+
+exampleDictionary2 = {'Jack': [15, 'blonde'], 'Bob': [22, 'brown'], 'Alice': [12, 'black']}
+print(exampleDictionary2['Jack'][1])
