2b9a88ea7355da564da546e4b00bad47749574d7
John <ionut_popovici92@yahoo.com>
2017-12-21 02:55:32 +0200

    Added built-in_functions.py

---
+++ Python_3_Basics/built-in_functions.py
@@ -0,0 +1,29 @@
+import math
+
+exampleNumber1 = -5
+exampleNumber2 = 5
+
+if abs(exampleNumber1) == exampleNumber2:
+    print('These are the same.')
+
+# Help
+
+exampleList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(min(exampleList))
+print(max(exampleList))
+
+x = 5.123
+print(round(x))
+x = 5.789
+print(round(x))
+
+print(math.floor(x))
+print(math.ceil(x))
+
+intMe = '55'
+print(intMe)
+print(int(intMe))
+print(float(intMe))
+
+strMe = 77
+print(str(strMe))
